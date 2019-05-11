23c516af8cffa2aa1167d8a4fc351bcf28d2d961
anindyakdp <deviandshin@gmail.com>
2019-05-11 14:14:28 +0700

    first commit

---
+++ app/src/main/java/com/example/class110519/Teman.kt
@@ -0,0 +1,31 @@
+package com.example.class110519
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.*
+
+class Teman : Fragment(){
+    companion object {
+        fun newInstance(): Teman {
+            return Teman()
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.add_friend, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        this.clearFindViewByIdCache()
+    }
+
+    }
